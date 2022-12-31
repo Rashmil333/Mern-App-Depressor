8ff4ce604cdcaaaf29c9fd9590a168eaa57677fc
rashmil333 <rashmilrajpoot333@gmail.com>
2022-12-31 02:31:43 -0800

    auth improvement

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,18 +5,20 @@ const auth=async(req,res,next)=>{
 	try{
 		// const token=req.cookies.jwt;
 		const token=req.headers['authorization'];
-		console.log(token);
+		console.log('1',token);
 		if(token){
 			token=token.split(' ')[1];
 		}
+		nsole.log('2',token);
 		const verifyUser=jwt.verify(token,process.env.SECRET_KEY);
-		// console.log(verifyUser);
+		console.log(verifyUser);
 		const rootUser=await Register.findOne({_id:verifyUser._id,"tokens.token":token});
-		// console.log("The rootUser is "+rootUser);
+		console.log("The rootUser is "+rootUser);
 		if(!rootUser){
 			throw new Error("User not found");
 		}
 		else{
+			nsole.log('1',"hhh->>>>>>");
 				req.token=token;
 				req.rootUser=rootUser;
 				req.userID=rootUser._id;
